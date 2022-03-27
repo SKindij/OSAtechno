e9746e2b99bf8cb72a9bd87dcba14b519932a7c7
SKindij <serhii.kindij@gmail.com>
2022-03-27 22:39:28 +0300

    Function for expanding a text block

--- OSA-JS/nav-osa.js
+++ OSA-JS/nav-osa.js
@@ -1,1 +1,14 @@
 //a feature that allows the user to change the background color of the page
+
+
+//Function for expanding a text block
+function showText (el) {
+   if(el.previousElementSibling.clientHeight === 40) {
+      el.previousElementSibling.style.height = "100%";
+      el.innerHTML = "ShowLess...";
+   } else {
+      el.previousElementSibling.style.height = "40px";
+      el.innerHTML = "Read More...";
+   }
+}
+
