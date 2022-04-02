d7459a668a568dee535491d240bf3791199e3061
SKindij <serhii.kindij@gmail.com>
2022-04-02 23:14:19 +0300

    I added a feature to change the background of the site page

--- OSA-JS/nav-osa.js
+++ OSA-JS/nav-osa.js
@@ -1,5 +1,16 @@
 //a feature that allows the user to change the background color of the page
-
+document.addEventListener('DOMContentLoaded', function() {
+   let body = document.querySelector('body');
+        document.querySelector('#backgr1').onclick = function() {
+           body.style.backgroundColor = 'Black';
+        };
+        document.querySelector('#backgr2').onclick = function() {
+           body.style.backgroundColor = 'PaleGoldenrod';
+        };
+        document.querySelector('#backgr3').onclick = function() {
+           body.style.backgroundColor = 'Maroon';
+        };  
+});
 
 //Function for expanding a text block
 function showText (el) {
