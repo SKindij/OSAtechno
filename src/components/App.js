caf0578326fe890ab7bd4e74e01ebadf79a28e35
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-04-06 15:00:33 +0300

    Create App.js

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Navigation from './navigation/Navigation';
+
+import './App.css';
+
+function App() {
+  return (
+    
+	<div>
+      <Navigation />
+      // Other components and content of your app
+    </div>
+	
+	
+  );
+}
+
+export default App;
