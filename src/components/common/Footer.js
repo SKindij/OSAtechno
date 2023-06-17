e4a51835440fc075e7871b7421e846d67351fba8
”SerhiiKindij” <serhii.kindij@gmail.com>
2023-06-17 16:29:28 +0300

    init: add necessary files
    components/common/Header & Footer

---
+++ src/components/common/Footer.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import vcss from '../../resources/commonImages/vcss.gif';
+import { Container, Row, Col  } from 'react-bootstrap';
+import { Form, Button, InputGroup } from 'react-bootstrap';
+
+const Footer = () => {
+  const handleRegistration = (event) => {
+    event.preventDefault();
+    // Виконайте необхідну логіку реєстрації
+    console.log(`User...`);
+  };
+
+  return (
+    <footer className="bg-light">
+        <Container className="mt-4" >
+          <h4>Login for registered users</h4>
+          <p>Provides access to professional functions on the portal.</p>
+          <Form onSubmit={handleRegistration}>
+            <Row className="align-items-center">
+              <Col sm={3} className="my-1">
+                <Form.Label htmlFor="formInputUsername" visuallyHidden>Username</Form.Label>
+                <InputGroup>
+                  <InputGroup.Text>User:</InputGroup.Text>
+                      <Form.Control id="formInputUsername" placeholder="nickname" />
+                    </InputGroup>
+                  </Col>
+                  <Col sm={3} className="my-1">
+                    <Form.Label htmlFor="formInputUserPassworde" visuallyHidden>Password:</Form.Label>
+                    <Form.Control id="formInputUserPassword" placeholder="Password" />
+                  </Col>
+                  <Col xs="auto" className="my-1">
+                    <Form.Check type="checkbox" id="autoSizingCheck2" label="Remember me" />
+                  </Col>
+                  <Col xs="auto" className="my-1">
+                    <Button type="submit">Enter</Button>
+                  </Col>
+                </Row>
+              </Form>
+            </Container>
+
+            <Container>
+               <Col>&copy; {new Date().getFullYear()} SKindij</Col>
+            </Container>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,4 +1,3 @@
-// Header.js
 import React from 'react';
 import { NavLink, Link } from 'react-router-dom';
 import { Navbar, Container, Nav } from 'react-bootstrap';
@@ -8,35 +7,54 @@ import './Common.scss';
 const Header = () => {
   return (
     <header className="header">
-      <Navbar bg="light" expand="lg" fixed="top">
-        <Container>
-          <Navbar.Brand>
-            <Link to="/" className="logo-link">
-              <img src={logo} alt="OSA-mini-logo" className="logo" />
-              <span className="brand-name">OSA - Optimal Selection of Accessories</span>
-            </Link>
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ml-auto">
-              <Nav.Link as={NavLink} to="/residential" className="nav-link" activeClassName="active">
-                Residential Gates
-              </Nav.Link>
-              <Nav.Link as={NavLink} to="/industrial" className="nav-link" activeClassName="active">
-                Industrial Gates
-              </Nav.Link>
-              <Nav.Link as={NavLink} to="/garageroller" className="nav-link" activeClassName="active">
-                Garage Rollers
-              </Nav.Link>
-              <Nav.Link as={NavLink} to="/windowroller" className="nav-link" activeClassName="active">
-                Window Rollers
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+      <Container>
+        <div className="d-flex justify-content-between align-items-center">
+          <Link to="/" className="logo-link">
+            <img src={logo} thumbnail="true" alt="OSA-mini-logo" className="logo" />
+          </Link>
+
+          <Navbar bg="light" expand="md">
+            <Container>
+
+              <Navbar.Brand>
+                <Link to="/" className="brand-name">OSA</Link>
+              </Navbar.Brand>
+
+              <Navbar.Toggle aria-controls="basic-navbar-nav" />
+              <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="ml-auto">
+                  <Nav.Link as={NavLink} to="/residential" className="nav-link" activeclassname="active">
+                    Residential Gates
+                  </Nav.Link>
+                  <Nav.Link as={NavLink} to="/industrial" className="nav-link" activeclassname="active">
+                    Industrial Gates
+                  </Nav.Link>
+                  <Nav.Link as={NavLink} to="/garageroller" className="nav-link" activeclassnamee="active">
+                    Garage Rollers
+                  </Nav.Link>
+                  <Nav.Link as={NavLink} to="/windowroller" className="nav-link" activeclassname="active">
+                    Window Rollers
+                  </Nav.Link>
+                </Nav>
+              </Navbar.Collapse>
+              
+            </Container>
+          </Navbar>
+        </div>
+
+        <div className="pb-3 text-center bg-light banner">
+          <div className="d-flex justify-content-center align-items-center flex-column">
+            <h2 className="mb-3">OSA - Optimal Selection of Accessories</h2>
+            <p className="mb-3">
+              Web application 🔮 designed to assist 👷 users 🔍 in finding ⭐ the best in terms of price-quality ratio accessories
+              for 🔧 upgrading, 🔨 repairing, or 🔱 enhancing various types of gates and roller shutters.
+            </p>
+          </div>
+        </div>
+      </Container>
     </header>
   );
 };
 
 export default Header;
+
