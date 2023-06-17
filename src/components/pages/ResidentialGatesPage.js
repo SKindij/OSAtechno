17518e25d2f4e24d7ed99b9f91a38c4a14583cf6
”SerhiiKindij” <serhii.kindij@gmail.com>
2023-06-17 16:42:03 +0300

    init: create necessary files
    add Rollers Pages

---
+++ src/components/pages/ResidentialGatesPage.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+
+const ResidentialGatesPage = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const handleProductClick = (productId) => {
+    navigate(`/residential/${productId}`);
+  };
+
+  const handleReplaceClick = () => {
+    navigate('/residential', { replace: true });
+  };
+
+  return (
+    <div>
+      <h2>Residential Gates</h2>
+      <p>Current location: {location.pathname}</p>
+      <p>Select a product:</p>
+      <ul>
+        <li onClick={() => handleProductClick('rd101')}>Product 101</li>
+        <li onClick={() => handleProductClick('rd102')}>Product 102</li>
+        <li onClick={() => handleProductClick('rd103')}>Product 103</li>
+        <li onClick={() => handleProductClick('rd104')}>Product 104</li>
+        <li onClick={() => handleProductClick('rd105')}>Product 105</li>
+      </ul>
+
+      <p>Go back to the previous action:</p>
+      <button onClick={() => navigate(-1)}>Go Back</button>
+      <p>Return to the beginning of the section:</p>
+      <button onClick={handleReplaceClick}>Replace</button>
+    </div>
+  );
+};
+
+export default ResidentialGatesPage;
---
+++ src/components/pages/WindowRollerPage.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+
+function WindowRollerPage() {
+ 
+
+return (
+      <div>
+	    View Window Roller Shutters
+	  </div>
+  );
+}
+
+
+export default WindowRollerPage;
