4a9485e2c7f08236674c1baeb62bbb8ac2d85d51
”SerhiiKindij” <serhii.kindij@gmail.com>
2023-06-18 00:58:39 +0300

    chore: refactor project structure for better organization

--- src/components/pages/MainPage.js
+++ src/components/pages/MainPage.js
@@ -12,55 +12,64 @@ import './Pages.scss';
 function MainPage() {
   return (
     <main className='main-page'>
-    <Container>
-      <Row xs={1} md={2} className="g-4 justify-content-center">
-        <Col>
-          <Card>
-            <Card.Img variant="top" src={residentialGateImg} />
-            <Card.Body>
-              <Card.Title>Residential Gates</Card.Title>
-              <Card.Text>Explore our range of residential gates.</Card.Text>
-              <Button as={Link} to="/residential" variant="primary">View Residential Gates</Button>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col>
-          <Card>
-            <Card.Img variant="top" src={industrialGateImg} />
-            <Card.Body>
-              <Card.Title>Industrial Gates</Card.Title>
-              <Card.Text>Discover our selection of industrial gates.</Card.Text>
-              <Button as={Link} to="/industrial" variant="primary">View Industrial Gates</Button>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-      <Row xs={1} md={2} className="g-4 justify-content-center">
-        <Col>
-          <Card>
-            <Card.Img variant="top" src={garageRollesImg} />
-            <Card.Body>
-              <Card.Title>Garage Roller Shutters</Card.Title>
-              <Card.Text>Discover our selection of garage roller shutters.</Card.Text>
-              <Button as={Link} to="/garageroller" variant="primary">View Garage Roller Shutters</Button>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col>
-          <Card>
-            <Card.Img variant="top" src={windowRolletsImg} />
-            <Card.Body>
-              <Card.Title>Window Roller Shutters</Card.Title>
-              <Card.Text>Discover our selection of window roller shutters.</Card.Text>
-              <Button as={Link} to="/windowroller" variant="primary">View Window Roller Shutters</Button>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-    </Container>
+      <Container>
+        <Row xs={1} md={2} className="g-4 justify-content-center">
+          <Col>
+            <Card className="card-wrapper">
+            <Card.Header>Garage gates</Card.Header>
+                <Card.Img variant="top" src={residentialGateImg} />
+              <Card.Body>
+                <Card.Title>Residential type</Card.Title>
+                <Card.Text>Explore our range of residential gates.</Card.Text>
+              </Card.Body>
+              <Card.Footer>
+                <Button as={Link} to="/residential" variant="outline-info">View Accessories</Button>
+              </Card.Footer>
+            </Card>
+          </Col>
+          <Col>
+            <Card className="card-wrapper">
+              <Card.Header>Garage gates</Card.Header>
+                <Card.Img variant="top" src={industrialGateImg} />
+              <Card.Body>
+                <Card.Title>Industrial type</Card.Title>
+                <Card.Text>Discover our selection of industrial gates.</Card.Text> 
+              </Card.Body>
+              <Card.Footer>
+                <Button as={Link} to="/industrial" variant="outline-info">View Accessories</Button>
+              </Card.Footer>
+            </Card>
+          </Col>
+          <Col>
+            <Card className="card-wrapper">
+              <Card.Header>Roller gates</Card.Header>
+                <Card.Img variant="top" src={garageRollesImg} />
+              <Card.Body>
+                <Card.Title>Residential type</Card.Title>
+                <Card.Text>Explore our range of garage roller shutters.</Card.Text>
+              </Card.Body>
+              <Card.Footer>
+                <Button as={Link} to="/garageroller" variant="outline-info">View Accessories</Button>
+              </Card.Footer>
+            </Card>
+          </Col>
+          <Col>
+            <Card className="card-wrapper">
+              <Card.Header>Aluminum shutters</Card.Header>
+                <Card.Img variant="top" src={windowRolletsImg} />
+              <Card.Body>
+                <Card.Title>Residential type</Card.Title>
+                <Card.Text>Discover our selection of window roller shutters.</Card.Text>
+              </Card.Body>
+              <Card.Footer>
+                <Button as={Link} to="/windowroller" variant="outline-info">View Accessories</Button>
+              </Card.Footer>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     </main>
   );
 }
 
 export default MainPage;
-
