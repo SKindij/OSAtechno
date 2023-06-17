baa3e845e8aa50e246d995b793945cd70d3ac42e
”SerhiiKindij” <serhii.kindij@gmail.com>
2023-06-17 13:38:22 +0300

    tyle: make whitespace adjustments

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.scss';
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
