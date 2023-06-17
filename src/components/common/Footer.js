4f077a80c7c9f2472db1d60061cf041b1e459700
”SerhiiKindij” <serhii.kindij@gmail.com>
2023-06-18 01:51:24 +0300

    style: update CSS styles for consistency

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -22,28 +22,28 @@ const Footer = () => {
                 <InputGroup>
                   <InputGroup.Text>User:</InputGroup.Text>
                       <Form.Control id="formInputUsername" placeholder="nickname" />
-                    </InputGroup>
-                  </Col>
-                  <Col md={3} className="my-1">
-                    <Form.Label htmlFor="formInputUserPassworde" visuallyHidden>Password:</Form.Label>
-                    <Form.Control id="formInputUserPassword" placeholder="Password" />
-                    <Form.Text className="text-muted">
-                      You can get your login and password from the portal administrator.
-                    </Form.Text>
-                  </Col>
-                  <Col xs="auto" className="my-1">
-                    <Form.Check type="checkbox" id="autoSizingCheck2" label="Remember me" />
-                  </Col>
-                  <Col xs="auto" className="my-1">
-                    <Button type="submit">Enter</Button>
-                  </Col>
-                </Row>
-              </Form>
-            </Container>
+                </InputGroup>
+              </Col>
+              <Col md={3} className="my-1">
+                <Form.Label htmlFor="formInputUserPassworde" visuallyHidden>Password:</Form.Label>
+                <Form.Control id="formInputUserPassword" placeholder="Password" />
+              </Col>
+              <Col xs="auto" className="my-1">
+                <Form.Check type="checkbox" id="autoSizingCheck2" label="Remember me" />
+              </Col>
+              <Form.Text className="text-muted">
+                You can get your login and password from the portal administrator.
+              </Form.Text>
+              <Col xs={6} className="my-1">
+                <Button type="submit"  variant="outline-success" style={{ width: '100%' }}>Enter</Button>
+              </Col>
+            </Row>
+          </Form>
+        </Container>
 
-            <Container>
-               <Col>&copy; {new Date().getFullYear()} SKindij</Col>
-            </Container>
+        <Container className="text-center">
+            <Col className="pt-4 copy">&copy; {new Date().getFullYear()} SKindij</Col>
+        </Container>
     </footer>
   );
 };
