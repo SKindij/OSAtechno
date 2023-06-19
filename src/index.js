dfc359adbe7d70ccbc93554c151f05f4aef637db
”SerhiiKindij” <serhii.kindij@gmail.com>
2023-06-19 19:55:12 +0300

    fix: patche bug in codebase
    refactor server rendering

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {BrowserRouter} from 'react-router-dom'
 
 import App from './App';
 import './index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  root.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+  );
