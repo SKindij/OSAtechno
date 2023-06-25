5364dda0d7be07130e2704b96d74da69f6076138
”SerhiiKindij” <serhii.kindij@gmail.com>
2023-06-25 15:47:58 +0300

    init: create an additional file
    add OrderConfirmation.js

--- src/components/order/OrderConfirmation.js
+++ src/components/order/OrderConfirmation.js
@@ -1,9 +1,13 @@
+//  - component functionality under development;
 import React from 'react';
 
 const OrderConfirmation = () => {
-    return (
-      < Component />
-    )
-}
+  return (
+    <div>
+      <h2>Order Confirmation</h2>
+      <p>Your order has been confirmed. Thank you!</p>
+    </div>
+  );
+};
 
- export default OrderConfirmation;
+export default OrderConfirmation;
