91448f42f1f0675305377970674dadf5a9e0cd90
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-06-27 13:15:01 +0300

    perf: make changes aimed at improving performance

--- src/components/pages/index.js
+++
@@ -1,8 +0,0 @@
-import MainPage from "./MainPage";
-import ResidentialGates from "./ResidentialGatesPage";
-import IndustrialGates from "./IndustrialGatesPage";
-import GarageRollerShutters from "./GarageRollerPage";
-import WindowRollerShutters from "./WindowRollerPage";
-import NotFoundPage from './NotFoundPage';
-
-export {MainPage, ResidentialGates, IndustrialGates, GarageRollerShutters, WindowRollerShutters, NotFoundPage};
