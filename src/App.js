58d890f733560742cf25f310ca46ef2b5d703c91
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-06-27 13:10:06 +0300

    perf: implement lazy loading for improved page load times
    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,17 +2,15 @@ import React from 'react';
 import { lazy, Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Header from "./components/common/Header";
+import MainPage from "./components/pages/MainPage";
 import Footer from "./components/common/Footer";
-import OrderForm from "./components/order/OrderForm";
 // with this type of import webpack looks in index.js in pages folder
-const MainPage = lazy(() => import('./components/pages/MainPage'));
 const ResidentialGates = lazy(() => import('./components/pages/ResidentialGatesPage'));
 const IndustrialGates = lazy(() => import('./components/pages/IndustrialGatesPage'));
 const GarageRollerShutters = lazy(() => import('./components/pages/GarageRollerPage'));
 const WindowRollerShutters = lazy(() => import('./components/pages/WindowRollerPage'));
 const Page404 = lazy(() => import('./components/pages/NotFoundPage'));
 const ProductDetails = lazy(() => import('./components/pages/ProductDetails'));
-
 function App() {
   return (
       <div className="app">
@@ -24,11 +22,8 @@ function App() {
             <Route path="/" element={<MainPage />} />
             <Route path="/residential" element={<ResidentialGates />} />
             <Route path="/industrial" element={<IndustrialGates />} />
-            <Route path="/residential/:productId" element={<ProductDetails />} />
-            <Route path="/industrial/:productId" element={<ProductDetails />} />
             <Route path="/garageroller" element={<GarageRollerShutters />} />
             <Route path="/windowroller" element={<WindowRollerShutters />} />
-            <Route path="/order" element={<OrderForm />} />
             <Route path="*" element={<Page404 />} />
           </Routes>
           </Suspense>
@@ -37,5 +32,4 @@ function App() {
       </div>
   );
 }
-
 export default App;
