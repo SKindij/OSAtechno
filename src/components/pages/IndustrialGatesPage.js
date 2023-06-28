97693fd1b0951bb7ef37821932e754866c9c4462
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-06-28 14:18:07 +0300

    chore: make general project maintenance changes
    Update IndustrialGatesPage.js

--- src/components/pages/IndustrialGatesPage.js
+++ src/components/pages/IndustrialGatesPage.js
@@ -1,18 +1,26 @@
-import React, { useState, useEffect, lazy, startTransition } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Helmet } from "react-helmet";
 import { Container, Row, Col, Button, Card, Form, ButtonGroup, ButtonToolbar, InputGroup } from 'react-bootstrap';
 import { FaEye } from 'react-icons/fa';
 import { BsFillBadgeAdFill } from 'react-icons/bs';
 import DataService from '../../services/DataService';
+import OrderForm from '../order/OrderForm';
+import ProductDetails from './ProductDetails';
 import './Pages.scss';
-const ProductDetails = lazy( () => import('./ProductDetails') );
-const IndustrialGatesPage = () => {
+
+const ResidentialGatesPage = () => {
   const [products, setProducts] = useState([]);
   const [filteredProducts, setFilteredProducts] = useState([]);
+	
   const [selectedProductId, setSelectedProductId] = useState(null);
   const [showProductDetails, setShowProductDetails] = useState(false);
-  const [quantity, setQuantity] = useState(0);
-  useEffect(() => {
+	
+  //const [selectedProductQuantity, setSelectedProductQuantity] = useState(0);	
+  const [selectedProductQuantities, setSelectedProductQuantities] = useState({});
+  const [selectedProducts, setSelectedProducts] = useState([]);
+  const [showOrderForm, setShowOrderForm] = useState(false);
+// function of obtaining goods from database
+  useEffect( () => {
     const fetchProducts = async () => {
       try {
         const industrialProducts = await DataService.getIndustrialProducts();
@@ -23,39 +31,81 @@ const IndustrialGatesPage = () => {
       }
     };
     fetchProducts();
-  }, []);
+  }, [] );
+// functionality of filtering groups of accessories
   const filterProducts = (category) => {
     if (category === 'All categories') {
       setFilteredProducts(products);
     } else {
-      const filtered = products.filter((product) => product.category === category);
+      const filtered = products.filter( (product) => product.category === category );
       setFilteredProducts(filtered);
     }
   };
-  const selectProduct = (productId) => {
+
+// functions of managing modal window of additional info about product
+  const selectProductId = (productId) => {
     setSelectedProductId(productId);
   };
   const handleOpenProductDetails = (productId) => {
     setSelectedProductId(productId);
-    startTransition(() => { setShowProductDetails(true); });
+    setShowProductDetails(true);
   };
   const handleCloseProductDetails = () => {
-    setSelectedProductId(null);
-    startTransition(() => { setShowProductDetails(false); });
+    setShowProductDetails(false);
   };
-  const handleQuantityChange = (event) => {
-    const value = parseInt(event.target.value, 10) || 0;
-    setQuantity(value);
+
+  // Update the quantity variable when the quantity changes
+const handleQuantityChange = (event, productId) => {
+  const value = parseInt(event.target.value, 10) || 0;
+  // Create a new object with the updated product quantity for the current ID
+  const updatedQuantities = {
+    ...selectedProductQuantities,
+    [productId]: value,
   };
-  const handleAddClick = async (event) => {
+  setSelectedProductQuantities(updatedQuantities);
+};
+// functionality for collecting desired goods
+  const handleAddClick = async (event, productId) => {
     event.preventDefault();
-    // Handle add button click event with quantity value
     try {
-      const selectedProduct = await DataService.getGatesById(selectedProductId);
+      const selectedProduct = await DataService.getGatesById(productId);
+      const quantity = selectedProductQuantities[productId] || 0;
+      // Updating the list of selected products
+      const updatedSelectedProducts = [...selectedProducts];
+      const existingProductIndex = updatedSelectedProducts.findIndex(
+        (product) => product.id === productId
+      );
+      if (existingProductIndex !== -1) {
+        // The product already exists, update its quantity
+        updatedSelectedProducts[existingProductIndex].quantity += quantity;
+      } else {
+        // Add a new product to the list
+        updatedSelectedProducts.push({
+          id: productId,
+          name: selectedProduct.name,
+          article: selectedProduct.article,
+          price: selectedProduct.price,
+          quantity: quantity,
+          unit: selectedProduct.unit,
+        });
+      }
+      setSelectedProducts(updatedSelectedProducts);
       console.log(`User add ${quantity} of ${selectedProduct.name}`);
     } catch (error) {
       console.error('Error fetching selected product:', error);
     }
+  };
+// functions of managing modal window of order form
+  const handleOpenOrderForm = () => {   
+    setShowOrderForm(true);
+  };
+  const handleCloseOrderForm = () => {   
+    setShowOrderForm(false);
+  };
+// clear all fields and remove visual effects from buttons
+  const handleClearSelectedAccessories = () => {
+    setSelectedProductQuantities({});
+    setSelectedProducts([]);
   };
 
   return (
@@ -71,8 +121,8 @@ const IndustrialGatesPage = () => {
         <link rel="canonical" href="https://osatechno.com/industrial" />
       </Helmet>
     <Container className={ `card-wrapper ${showProductDetails ? 'blur' : ''}` }>
-        <h1>Accessories for industrial sectional doors</h1>
-        <Row className="mb-4">       
+      <h1>Accessories for industrial sectional doors</h1>
+      <Row className="mb-4">       
         <Col>
           <ButtonToolbar aria-label="Product categories">
             <ButtonGroup className="me-2">
@@ -81,13 +131,16 @@ const IndustrialGatesPage = () => {
               </Button>
             </ButtonGroup>
             <ButtonGroup>
-              <Button variant="info" onClick={() => filterProducts('On Shaft')} className="me-2">
+              <Button variant="info" aria-label="Filter On Shaft"
+                onClick={() => filterProducts('On Shaft')} className="me-2">
                 On Shaft
               </Button>{' '}
-              <Button variant="info" onClick={() => filterProducts('On Panel')} className="me-2">
+              <Button variant="info" aria-label="Filter On Panel"
+                onClick={() => filterProducts('On Panel')} className="me-2">
                 On Panel
               </Button>{' '}
-              <Button variant="info" onClick={() => filterProducts('Railsystem')} className="me-2">
+              <Button variant="info" aria-label="Filter for Railsystem"
+                onClick={() => filterProducts('Railsystem')} className="me-2">
                 Railsystem
               </Button>
             </ButtonGroup>
@@ -95,15 +148,18 @@ const IndustrialGatesPage = () => {
         </Col>
       </Row>
       <Row>
-      {filteredProducts.map( (product) => (
+       {filteredProducts.map( (product) => {        
+         const quantity = selectedProductQuantities[product.id] || 0;
+        return (
         <Col key={product.id} xs={12} md={6} lg={4} xl={3}>
           <Card className={`product-card ${selectedProductId === product.id ? 'product-card-highlight' : ''}`}
-            onClick={() => selectProduct(product.id)}
+            onClick={() => selectProductId(product.id)}
           >
             <Card.Header>{product.name}</Card.Header>
             <div className="product-image-container">
-              <Card.Img className="product-image" variant="top"
-                src={product.imageA} alt={product.name} />
+              <Card.Img className="product-image" variant="top" 
+                src={product.imageA} alt={product.name}
+                width="500" height="375"/>
             </div>
             <Card.Body>
               <Card.Text>Article: {product.article}</Card.Text>
@@ -117,13 +173,14 @@ const IndustrialGatesPage = () => {
                     <Form.Control aria-label="Need for spare parts"
                       type="number" min={0} step={1} pattern="[0-9]+"
                       inputMode="numeric"
-                      onChange={handleQuantityChange}
+                      value={quantity === 0 ? '' : quantity}
+                      onChange={(event) => handleQuantityChange(event, product.id)}
                     />
                     <InputGroup.Text>{product.unit}</InputGroup.Text>
-                    <Button variant="outline-secondary" 
-                      onClick={handleAddClick}
-                      aria-label="Add value to cart">
-                      <BsFillBadgeAdFill />
+                    <Button variant='outline-warning'
+                      onClick={(event) => handleAddClick(event, product.id)}
+                      aria-label="Add item to Selected Products">
+                        <BsFillBadgeAdFill />
                     </Button>
                   </InputGroup>
                   </Form.Group>
@@ -139,13 +196,33 @@ const IndustrialGatesPage = () => {
             </Card.Footer>
           </Card>
         </Col>
-      ) )}        
+       );
+      } )}        
       </Row>
       {selectedProductId && showProductDetails && (
-        <ProductDetails onClose={handleCloseProductDetails} productId={selectedProductId} />
+        <ProductDetails 
+          onClose={handleCloseProductDetails} 
+          productId={selectedProductId} />
       )}
+      <div className="d-grid gap-2">
+      <Button variant="outline-success" size="lg"
+        onClick={() => handleOpenOrderForm()}
+        aria-label="Open windwow with order form">
+          Open windwow with order form
+      </Button>
+      <Button variant="outline-danger" size="lg"
+        onClick={handleClearSelectedAccessories}>
+          Clear the list of selected accessories
+      </Button>
+      </div>             
+      {showOrderForm && (
+        < OrderForm 
+          onClose={handleCloseOrderForm} 
+          selectedProducts={selectedProducts} 
+          setSelectedProducts={setSelectedProducts} />
+      )}   
     </Container>
     </main>
   );
 };
-export default IndustrialGatesPage;
+export default ResidentialGatesPage;
